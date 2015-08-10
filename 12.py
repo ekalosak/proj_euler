ebb34018c982ae7b7e2ad687ace4fd489e06151d
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-10 14:13:43 -0400

    refactored prime number finding and triangle number finding into their own modules

--- 12.py
+++ 12.py
@@ -3,48 +3,16 @@ Author: eric kalosa-kenyon 8-9-15
 Problem: first triangle to have over 500 divsors
 '''
 
+# sys imports
 import pdb
 
+# math imports
 import math
 import numpy as np
-import functools as ft
 
-def tri_next(t, n):
-    ''' given the n'th triangle number t, make the next one. '''
-    return (t + n + 1, n + 1)
-
-def tri(n):
-    ''' find the n'th triange number '''
-    t1 = 1
-    n1 = 1
-    while n1 < n:
-        t1, n1 = tri_next(t1, n1)
-    return t1
-
-def prime(upto=100):
-    ''' http://rebrained.com/?p=458 '''
-    return [1] + filter(lambda num: (num %
-        np.arange(2,1+int(math.sqrt(num)))).all(), range(2,upto+1))
-
-'''
-def prime_factors(n):
-    pfac = []   # List of tuples (e, p) by FTA
-    primes = prime(int(math.sqrt(n)) + 1)
-    w = 0
-    p = primes[w]
-    while n >= p**2:
-        e = 0
-        while not n % p:    # while n still divides p
-            e = e + 1
-            n = n / p
-        if e:
-            pfac = pfac + [(e, p)]
-        w = w + 1
-        p = primes[w]
-    if n > 1:
-        pfac = pfac + [(1, n)]
-    return pfac
-'''
+# local imports
+from primes import prime
+from triangle import tri
 
 def prime_factors(n):
     # not including 1
@@ -70,8 +38,6 @@ def num_divisors(t):
     ''' find the number of divisors (inc t) of t '''
     pf = prime_factors(t)
     je = just_exp(pf)
-    if len(je) == 1:
-        return je[0][0] + 1
     return reduce(lambda e1, e2: (e1 + 1)*(e2 + 1), je)
 
 def tests():
