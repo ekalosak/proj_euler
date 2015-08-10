33d29e234ae26a5572b6884d05681f0d67e51b64
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-10 15:28:06 -0400

    Wrote foolishly with stately triangle numbers...

--- 12.py
+++ 12.py
@@ -11,56 +11,30 @@ import math
 import numpy as np
 
 # local imports
-from primes import prime
-from triangle import tri
+from triangle import tri_state
+from divisors import num_divisors
 
-def prime_factors(n):
-    # not including 1
-    pfac = []   # List of tuples (e, p) by FTA
-    primes = prime(int(math.sqrt(n)) + 1)
-    for p in primes:
-        if p == 1: continue
-        e = 0
-        while not n % p:    # while n still divides p
-            e = e + 1
-            n = n / p   # chop away at the prime factorization
-        if e:
-            pfac = pfac + [(e, p)]
-    if n > primes[-1]: pfac = pfac + [(1,n)]    # if n was prime to begin with
-    return pfac
+def main(N):
+    # want the first triangle number with more than N divisors
+    T = []
+    ndiv = 1
+    n = 0
+    ndivmax = 1
+    while ndiv <= N:  # Until this loop reaches its goal
+        T = tri_state(T)
+        t = T[-1]
+        ndiv = num_divisors(t)
+        n = n + 1
+        if ndiv > ndivmax:
+            ndivmax = ndiv
+            print n, t, ndiv
+    print t, ' has ', ndiv, ' divisors.'
+    return t
 
-def just_exp(pfac):
-    ''' given the list of prime factor exponent pairs [(e, p)..], return [e..]
-    '''
-    return map(lambda pe: pe[0], pfac)
-
-def num_divisors(t):
-    ''' find the number of divisors (inc t) of t '''
-    pf = prime_factors(t)
-    je = just_exp(pf)
-    return reduce(lambda e1, e2: (e1 + 1)*(e2 + 1), je)
-
-def tests():
-    assert prime(10) == [1, 2, 3, 5, 7]
-    assert tri(4) == 10
-    assert (2,2) in prime_factors(20) and (1,5) in prime_factors(20)
-    assert num_divisors(20) == 6    # |{1, 2, 4, 5, 10, and 20}| = 6
-    print prime_factors(3), just_exp(prime_factors(3)), num_divisors(3)
-    pdb.set_trace()
+def test():
+    assert main(5) == 28
     print "Passed tests!!"
 
-def main():
-    tests()
-    t = 1   # the triangle number t is 1 for n = 1
-    n = 1
-    N = 500 # want the first triangle number with more than 500 divisors
-    nfac = 1    # 1 has 1 divisor, base case
-    print n, t, nfac
-    while nfac <= 500:  # Until this loop reaches its goal
-        t, n = tri_next(t, n)   #   get the next triange number
-        nfac = num_divisors(t)
-        print n, t, nfac
-    print t, ' has ', nfac, ' divisors.'
-
 if __name__ == '__main__':
-    main()
+    test()
+    main(500)
