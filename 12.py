ef87b33d83649b67b6d929df757a8cf0a6d0d4de
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-10 15:40:02 -0400

    added benchmarking featurelet

--- 12.py
+++ 12.py
@@ -5,35 +5,47 @@ Problem: first triangle to have over 500 divsors
 
 # sys imports
 import pdb
+import time
 
 # math imports
 import math
 import numpy as np
 
 # local imports
-from triangle import tri_state
+from triangle import tri
 from divisors import num_divisors
 
+def benchmark(fxn):
+
+    def w_bmk(*args, **kw):
+        t0 = time.clock()
+        r = fxn(*args, **kw)
+        t1 = time.clock()
+        print "Function took ", t1-t0, " seconds."
+        return r
+
+    return w_bmk
+
+@benchmark
 def main(N):
     # want the first triangle number with more than N divisors
-    T = []
     ndiv = 1
-    n = 0
+    n = 1
     ndivmax = 1
     while ndiv <= N:  # Until this loop reaches its goal
-        T = tri_state(T)
-        t = T[-1]
+        t = tri(n)
         ndiv = num_divisors(t)
-        n = n + 1
         if ndiv > ndivmax:
             ndivmax = ndiv
             print n, t, ndiv
+        n = n + 1
     print t, ' has ', ndiv, ' divisors.'
     return t
 
 def test():
     assert main(5) == 28
     print "Passed tests!!"
+
 
 if __name__ == '__main__':
     test()
