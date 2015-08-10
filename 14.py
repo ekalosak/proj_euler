172b03d97c3a56b4706ac10e1f92744f82edaf93
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-10 14:08:48 -0400

    outlined the collatz problem (14)

---
+++ 14.py
@@ -0,0 +1,13 @@
+'''
+Author: eric kalosa-kenyon 
+Problem: find number under 1m that has longest collatz chain
+'''
+
+import math as ma
+import numpy as np
+
+def main():
+    ''' '''
+
+if __name__ == '__main__':
+    main()
