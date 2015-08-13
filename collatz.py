b5989c60ef77cab082df94d98c7b74899d89eb8a
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-12 22:47:04 -0400

    Fleshed out the collatz file and added list value pushing to the stack. Still not working

--- collatz.py
+++ collatz.py
@@ -1,12 +1,22 @@
 from tree import tree
+from stack import stack
 
 def nxt(n):
+    ''' return the next value in the Collatz sequence '''
     if n <= 0: raise ValueError("Can't handle non-natural values")
     elif n == 1: return 1
     else:
         if n % 2 == 0: return n/2
         else: return 3*n + 1
 
+def prev(n):
+    ''' go backwards in the Collatz sequence '''
+    if n <= 0: raise ValueError("Can't handle non-natural values")
+    if (n - 1) % 3 == 0:
+        return [(n-1)/3, 2*n]
+    else:
+        return [2*n]
+    
 def seq(n):
     s = [n]
     while n != 1:
@@ -29,17 +39,36 @@ def maxlen1(N):
     return r
 
 def maxlen2(N):
-    r = {}
-    ''' calculate collatz tree backwards build same kind of structure as ml1
-    above '''
-    n = 1
+    ''' calculate collatz tree backwards brutishly
+    Initialize a list from 1..N
+    Calculate backwards from 1 -> 2 -> 4 -> (1,8) etc. 
+    Once a number is encountered, add it to a dict with number
+        and length of chain.
+    When a number is encountered, add it to a stack
+    Return (n, l) number and length of number with longest
+        chain under N (n<N)'''
+    r = {1: 1, 2: 2, 4: 3}  # records lengths of chain, avoid obv cycle
+    s = stack([8])  # holds numbers to be computed
+    t = range(1,N)
+    while len(t) > 0:
+        n = s.pop() # get a number that has yet to have its len calcd
+        nx = nxt(n) # check what its parent in the tree is
+        r[n] = r[nx] + 1  # n's length is 1 + parent's length 
+
+        if n in t:
+            t.remove(n) # so we know when we've calculated lens for all nums
+                        # under N
+        s.push(prev(n)) # set up the previous elements in the seq
+
 
 def test():
     assert nxt(3) == 10
     assert nxt(2) == 1
     assert nxt(1) == 1
     assert seq(3) == [3, 10, 5, 16, 8, 4, 2, 1]
-    print "Tests passed!!"
+    assert maxlen2(2) == (1, 1)
+    assert maxlen2(10) == (9, len(seq(9)))
+    print "Tests passed!!, maxlen1 not tested"
 
 if __name__ == '__main__':
     test()
--- stack.py
+++ stack.py
@@ -1,9 +1,30 @@
 class stack:
 
-    def __init__(self):
-        self.c = []
+    def __init__(self, vals = []):
+        self.c = vals
 
-    def pop(a):
+    def pop(self):
         return self.c.pop()
-    def push(a):
-        self.c = [a] + self.c
+    def push(self, a):
+        if type(a) == type([]):
+            self.c = a + self.c
+        else:
+            self.c = [a] + self.c
+    def vals(self):
+        return self.c
+
+    def __str__(self):
+        return str(self.c)
+
+def test():
+    s = stack(range(3))
+    assert s.pop() == 2
+    assert s.vals() == [0, 1]
+    s.push(1)
+    assert s.vals() == [1, 0, 1]
+    s.push([4, 5])
+    assert s.vals() == [4, 5, 1, 0, 1]
+    print "Passed tests!!"
+
+if __name__ == '__main__':
+    test()
