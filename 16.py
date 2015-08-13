c30d8b2ac69bb1c8360a8579492d8205746f1394
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-08-13 10:42:34 -0400

    solved lattice paths combinitorially

---
+++ 16.py
@@ -0,0 +1,21 @@
+'''
+Author: eric kalosa-kenyon 
+Problem: 16, sum digits of 2^1000
+'''
+
+import math as ma
+import numpy as np
+
+def sumDig(N):
+    ''' sums digits of 2^N '''
+    pass    # TODO
+
+def main():
+    ''' '''
+
+def test():
+    print "Passed tests!!"
+
+if __name__ == '__main__':
+    test()
+    main()
--- latticePaths.py
+++ latticePaths.py
@@ -5,19 +5,26 @@ Problem: 15 - find lattice paths in 20x20 grid
 
 import math as ma
 import numpy as np
+from scipy.misc import comb
+
+''' finds the number of lattice paths in an NxM grid
+how: have to go down N times, over M times, no backtracing
+    so count the ways e.g. (o,d,d,o) N d's and M o's
+    can be rearranged uniquely '''
+
+''' Naievely, 1xN has N+1 solutions, so 2xN has, summed over all i = 0 to N,
+(N-i)*(N - (N-i) + 1). Similarly, 3xN has, summed over all i = 0 to N, j = i
+to N, ...? the previous formula seems suspect '''
+
+''' Apparently we can do this with N+M choose M '''
 
 def numPaths(N, M):
-    ''' finds the number of lattice paths in an NxM grid
-    how: have to go down N times, over M times, no backtracing
-        so count the ways e.g. (o,d,d,o) N d's and M o's
-        can be rearranged uniquely '''
-
-    ''' Naievely, 1xN has N+1 solutions, so 2xN has, summed over all i = 0 to N,
-    (N-i)*(N - (N-i) + 1). Similarly, 3xN has, summed over all i = 0 to N, j = i
-    to N, ...? the previous formula seems suspect '''
+    ''' finds num paths in an NxM lattice without analytic formula '''
+    return int(comb(N + M, M))
 
 def main():
     ''' '''
+    print numPaths(20, 20)
 
 def test():
     assert numPaths(2, 2) == 6
