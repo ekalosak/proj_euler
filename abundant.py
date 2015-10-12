fd3a7e56d139b94e4e1a2e47e99a7f88e4e450e6
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-10-12 13:45:07 -0400

    added a more efficient method for checking sums (reverse self iteration) and state memoization

--- abundant.py
+++ abundant.py
@@ -15,6 +15,12 @@ import pdb
 import math
 from divisors import div
 import itertools as itt
+import pickle
+import os
+import sys
+
+FN = "./abundant_numbers.p"
+MEM = "./abundant_memoization.p"
 
 def abundant(n):
     # returns true if n is abundant
@@ -30,23 +36,44 @@ def abundant(n):
 
 def find_abundant(N):
     # returns a list of abundant numbers less than N
-    return [k for k in range(1, N) if abundant(k)]
+
+    sufficient = False
+    if os.path.exists(FN):
+        print "Loading abundant numbers"
+        pa = pickle.load(open(FN, "rb"))
+        if N <= pa[0]:
+            print "There were sufficiently many in the previous calculation"
+            abts = [a for a in pa[1] if a <= N]
+        else:
+            print "There were not enough in the previous calculation"
+            abts = [k for k in range(1, N) if abundant(k)]
+            pa = N, abts
+            pickle.dump(pa, open(FN, "wb"))
+    else:
+        print "Calculating abundant numbers"
+        abts = [k for k in range(1, N) if abundant(k)]
+        pa = N, abts
+        pickle.dump(pa, open(FN, "wb"))
+
+    return abts
+
+def reverse_self_iter(ns):
+    k = 0
+    while k < len(ns):
+        j = k
+        while j < len(ns) - k:
+            try:
+                yield ns[k], ns[-j]
+            except IndexError as e:
+                pdb.set_trace()
+            j = j + 1
+        k = k + 1
 
 def is_sum_of_abundant(n, abdnts):
     # returns true if n = a + b where a and b are abundant
     qq = [q for q in abdnts if q <= n]
-    ww = [w for w in abdnts if w <= n/2]
-    ee = [e for e in abdnts[::-1] if e > n/2]
     # for a, b in itt.product(qq, qq):
-    for a, b in itt.product(ww, ee):
-        # print "is {} + {} == {}?".format(a, b, n),
-        if n == a + b:
-            # print "\tYes!"
-            return a, b
-        # else:
-            # print "\tNo."
-
-    for a, b in itt.product(qq, qq):
+    for a, b in reverse_self_iter(qq):
         if n == a + b:
             return a, b
     return 0, 0
@@ -68,19 +95,32 @@ def main():
     print "found {} abundant numbers".format(len(abts))
     # sabts = [k for k in range(1, N) if is_sum_of_abundant(k, abts)]
     print "finding numbers that are sums of abundant numbers"
-    sabts = []
-    nsabts = []
-    for k in range(1, N):
-        print "testing {}".format(k),
-        a, b = is_sum_of_abundant(k, abts)
-        if a or b:
-            # print "{} is the sum of abundant numbers {} + {}".format(k, a, b)
-            print "\tpassed"
-            sabts.append(k)
-        else:
-            # print "{} is not the sum of abundant numbers".format(k)
-            print "\tfailed"
-            nsabts.append(k)
+
+    if os.path.exists(MEM):
+        start, sabts, nsabts = pickle.load(open(MEM, "rb"))
+    else:
+        start = 0
+        sabts = []
+        nsabts = []
+
+    try:
+        for k in range(start, N):
+            print "testing {}".format(k),
+            a, b = is_sum_of_abundant(k, abts)
+            if a or b:
+                # print "{} is the sum of abundant numbers {} + {}".format(k, a, b)
+                print "\tpassed: {} + {}".format(a, b)
+                sabts.append(k)
+            else:
+                # print "{} is not the sum of abundant numbers".format(k)
+                print "\tfailed"
+                nsabts.append(k)
+
+    except KeyboardInterrupt as ke:
+        print "\npickling progress: {} at {}".format(k, MEM)
+        pickle.dump([k, sabts, nsabts], open(MEM, "wb"))
+        print "pickled!"
+        sys.exit(1)
 
     print "The sum of all the positive integers that cannot",
     print " be written as the sum of two abundant numbers is {}".format(
@@ -88,4 +128,7 @@ def main():
 
 if __name__ == '__main__':
     test()
-    main()
+    try:
+        main()
+    except Exception as ee:
+        pdb.set_trace()
